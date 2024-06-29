3f0ab62a2835c4ccf1e653af9c37b962cf2d2bb2
Paolo Tardioli <Felp4to@users.noreply.github.com>
2024-06-29 22:54:14 +0200

    Training LSTM e GRU

---
+++ settings.py
@@ -0,0 +1,10 @@
+import constants as cs
+import yaml
+
+
+def read_yaml_fields():
+    with open(cs.YAMLFILE, 'r') as ff:
+        content = yaml.safe_load(ff)
+        a = set(content.get('actions', []))
+        c = content.get('counter', {})
+        return a, c
